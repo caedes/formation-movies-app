b0f30b5b12764a1c72fdb9ce7d31cfa17e99fb39
Romain Laurent <romain.laurent@cdiscount.com>
2022-03-07 14:20:58 +0100

    feat: add app version

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import _ from "lodash";
 
 import "./style.css";
+import { version } from "../package.json";
 import Dices from "./dices.png";
 
 function component() {
   const element = document.createElement("div");
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "Romain"], " ");
+  element.innerHTML = _.join(["Hello", "Romain", `v${version}`], " ");
   element.classList.add("hello");
 
   const $dices = new Image();
